60a518f08c6a1df25d6a3ef71b739ffc124fd54f
Koustav Ram Mani <42280792+Koustav7890@users.noreply.github.com>
2021-04-25 20:05:52 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react"
+import Display from "./Components/Display"
+
+function App() {
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
+}
+
+export default App;
